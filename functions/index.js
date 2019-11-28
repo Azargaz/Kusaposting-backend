36a9f0902fc06507cb9dd28a9233aaf1f8df220a
azargaz <huba310@gmail.com>
2019-11-28 17:53:09 +0100

    Added login through Firebase auth.

--- functions/index.js
+++ functions/index.js
@@ -50,6 +50,17 @@ app.post('/kusopost', (req, res) => {
 		})
 });
 
+const isEmpty = (string) => {
+	if(string.trim() === '') return true;
+	else return false;
+};
+
+const isEmail = (email) => {
+	const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(email.match(regEx)) return true;
+	else return false;
+};
+
 // Signup route
 app.post('/singup', (req, res) => {
 	const newUser =	{
@@ -58,6 +69,20 @@ app.post('/singup', (req, res) => {
 		confirmPassword: req.body.confirmPassword,
 		handle: req.body.handle
 	};
+
+	let errors = {};
+
+	if(isEmpty(newUser.email)) {
+		errors.email = 'must not by empty';
+	} else if(!isEmail(newUser.email)) {
+		errors.email = 'must be a valid email address';
+	}
+
+	if(isEmpty(newUser.password)) errors.password = 'must not be empty';
+	if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'passwords must match';
+	if(isEmpty(newUser.handle)) errors.handle = 'must not be empty';
+
+	if(Object.keys(errors).length > 0) return res.status(400).json(errors);
 
 	let token, userId;
 	db.doc(`/users/${newUser.handle}`).get()
@@ -94,6 +119,35 @@ app.post('/singup', (req, res) => {
 			} else {
 				return res.status(500).json({ error: err.code });
 			}			
+		});
+});
+
+app.post('/login', (req, res) => {
+	const user = {
+		email: req.body.email,
+		password: req.body.password
+	};
+
+	let errors = {};
+
+	if(isEmpty(user.email)) errors.email = 'must not be empty';
+	if(isEmpty(user.password)) errors.password = 'must not be empty';
+
+	if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+	firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+		.then(data => {
+			return data.user.getIdToken();
+		})
+		.then(token => {
+			return res.json({ token });
+		})
+		.catch(err => {
+			console.error(err);
+			if(err.code === 'auth/wrong-password') {
+				return res.status(403).json({ general: 'wrong credentials, please try again' });
+			} else 
+				return res.status(500).json({ error: err.code });
 		});
 });
 
